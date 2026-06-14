740040b6b12eb1c0468c8a62b34c14af538b38d4
BilyQuim <binbilly885@gmail.com>
2026-06-13 21:56:38 -0600

    Fix GraphQL route handling

--- Services/graphql-api/app/main.py
+++ Services/graphql-api/app/main.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(graphql_router.router, prefix="/graphql")
+app.include_router(graphql_router.router)
 
 @app.get("/", tags=["Raíz"])
 def leer_raiz():
